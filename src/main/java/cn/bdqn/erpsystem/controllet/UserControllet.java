01bf6b5d166396a3dee6665dcf601d3b7ec02548
yunlung <759414288@qq.com>
2019-11-02 22:43:34 +0800

    init.02

--- src/main/java/cn/bdqn/erpsystem/controllet/UserControllet.java
+++ src/main/java/cn/bdqn/erpsystem/controllet/UserControllet.java
@@ -3,18 +3,16 @@ package cn.bdqn.erpsystem.controllet;
 import com.alibaba.fastjson.JSONArray;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class UserControllet {
     @RequestMapping("/init")
     public String init(){
-        return "index";
+        return "tables_lists";
     }
 
-    @ResponseBody
     @RequestMapping("/list")
     public String table_lists(){
-        return JSONArray.toJSONString("table_content");
+        return JSONArray.toJSONString("table_lists");
     }
 }
