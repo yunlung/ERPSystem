b3213c259deae9b07752dc7ca02dceb833bc4b3c
yunlung <759414288@qq.com>
2019-11-05 15:29:50 +0800

    init.04

---
+++ src/main/java/cn/bdqn/erpsystem/dao/ErpUserMapper.java
@@ -0,0 +1,30 @@
+package cn.bdqn.erpsystem.dao;
+
+import cn.bdqn.erpsystem.pojo.ErpUser;
+import cn.bdqn.erpsystem.pojo.ErpUserExample;
+import java.util.List;
+import org.apache.ibatis.annotations.Param;
+
+public interface ErpUserMapper {
+    int countByExample(ErpUserExample example);
+
+    int deleteByExample(ErpUserExample example);
+
+    int deleteByPrimaryKey(Integer uId);
+
+    int insert(ErpUser record);
+
+    int insertSelective(ErpUser record);
+
+    List<ErpUser> selectByExample(ErpUserExample example);
+
+    ErpUser selectByPrimaryKey(Integer uId);
+
+    int updateByExampleSelective(@Param("record") ErpUser record, @Param("example") ErpUserExample example);
+
+    int updateByExample(@Param("record") ErpUser record, @Param("example") ErpUserExample example);
+
+    int updateByPrimaryKeySelective(ErpUser record);
+
+    int updateByPrimaryKey(ErpUser record);
+}
---
+++ src/main/java/cn/bdqn/erpsystem/pojo/ErpUser.java
@@ -0,0 +1,43 @@
+package cn.bdqn.erpsystem.pojo;
+
+public class ErpUser {
+    private Integer uId;
+
+    private String uName;
+
+    private String uPassword;
+
+    private String uType;
+
+    public Integer getuId() {
+        return uId;
+    }
+
+    public void setuId(Integer uId) {
+        this.uId = uId;
+    }
+
+    public String getuName() {
+        return uName;
+    }
+
+    public void setuName(String uName) {
+        this.uName = uName == null ? null : uName.trim();
+    }
+
+    public String getuPassword() {
+        return uPassword;
+    }
+
+    public void setuPassword(String uPassword) {
+        this.uPassword = uPassword == null ? null : uPassword.trim();
+    }
+
+    public String getuType() {
+        return uType;
+    }
+
+    public void setuType(String uType) {
+        this.uType = uType == null ? null : uType.trim();
+    }
+}
