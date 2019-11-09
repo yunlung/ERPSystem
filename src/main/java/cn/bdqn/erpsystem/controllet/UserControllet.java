2a59d137fe8db074beca97448ff2c3c0dee84646
yunlung <759414288@qq.com>
2019-11-09 12:31:33 +0800

    init.1

--- src/main/java/cn/bdqn/erpsystem/controllet/UserControllet.java
+++ src/main/java/cn/bdqn/erpsystem/controllet/UserControllet.java
@@ -7,31 +7,49 @@ import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import javax.annotation.Resource;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 
 @Controller
 @RequestMapping("user")
 public class UserControllet {
     @Resource
     private UserService userService;
-    //用户展示也
+    //用户展示页
     @RequestMapping("/list")
     public String init(Model model){
         model.addAttribute("userList",userService.findAllErpUser());
         return "table_users";
     }
     //转到用户增加页
-    @RequestMapping("/toAdd")
+    @RequestMapping(value = "/toAdd",method = RequestMethod.GET)
     public String toAdd(){
         return "add_user";
     }
-
-    public String addUser(ErpUser erpUser,Model model){
+    @RequestMapping(value = "/toAdd",method = RequestMethod.POST)
+    public String addUser(ErpUser erpUser,@Param("password")String password,
+                          @Param("birthday")String birthday,
+                          Model model){
+        erpUser.setuPassword(password);
+        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        try {
+            erpUser.setuBirthday(simpleDateFormat.parse(birthday));
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
         if (userService.addErpUser(erpUser)){
             return "redirect:/user/list";
         }
         model.addAttribute("addError","增加失败!");
         return "add_user";
+    }
+    @RequestMapping("/getUserbyId")
+    @ResponseBody
+    public String getUserById(){
+        return "";
     }
 }
---
+++ src/main/resources/static/js/userBusiness.js
@@ -0,0 +1,34 @@
+function updateUser(id,obj) {
+    $.ajax({
+        "url":"/getUserbyId",
+        "data":{"uId":id},
+        "type":"post",
+        "dataType":"json",
+        success:function (data) {
+            if (date!=null){
+                // $(obj).parent().remove();
+                location.href="/user/toAdd";
+            }
+        },
+        error:function (e) {
+            alert("数据错误!");
+        }
+    });
+}
+function deleteUser(id,obj) {
+    $.ajax({
+        "url":"/delete",
+        "data":{"uId":id},
+        "type":"get",
+        "dataType":"json",
+        success:function (data) {
+            if (date!=null){
+                $(obj).parent().parent().remove();
+                // location.href="/user/toAdd";
+            }
+        },
+        error:function (e) {
+            alert("数据错误!");
+        }
+    });
+}
